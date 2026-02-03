123040aafd3f2bc1c4f693bfcf23b35e97c3b72f
Christina <christinaraganit@gmail.com>
2026-02-03 02:16:06 -0800

    personal website

---
+++ scripts/main.js
@@ -0,0 +1,2 @@
+// main.js
+// This file is mandatory and should be used for adding basic JavaScript interactivity.
---
+++ scripts/vis.js
@@ -0,0 +1,2 @@
+// vis.js
+// This file will contain the code for generating SVG drawings and visualization.
